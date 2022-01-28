63b8cc98ec6fa627003eb43f8f6fac719b4c1867
Seraph <42664208+Seraph27@users.noreply.github.com>
2022-01-28 12:33:50 -0800

    fixes test file 4

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -45,6 +45,9 @@ public class MarkdownParse {
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
+
+
+            if (nextCloseBracket > openParen) break;  
             
             if (!checkExtension(markdown.substring(openParen +1, closeParen)) && openParen-nextCloseBracket==1)
             {
